01f9759bca609e57b8702aae24eba770d31ed761
Gustavo Vieira <gmtnvieira@gmail.com>
2020-07-16 13:26:49 -0300

    initial push

---
+++ supernaturalinheritancecodeacad/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MagicalInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Storm[] storms = new Storm[10];
+            int stormsIndex = 0;
+
+
+
+            Pupil p = new Pupil("Mezil-kree", "Icecrown");
+            storms[stormsIndex] = p.CastWindStorm();
+            stormsIndex++;
+
+            Mage mag = new Mage("Gul'dan", "Draenor");
+            storms[stormsIndex] = mag.CastRainStorm();
+            stormsIndex++;
+
+            Archmage arc = new Archmage("Nielas Aran", "Stormwind");
+            storms[stormsIndex] = arc.CastWindStorm();
+            stormsIndex++;
+            storms[stormsIndex] = arc.CastRainStorm();
+            stormsIndex++;
+            storms[stormsIndex] = arc.CastLightningStorm();
+            stormsIndex++;
+
+            for (int i = 0; i < stormsIndex; i++)
+            {
+                Console.WriteLine(storms[i].Announce());
+            }
+
+
+        }
+    }
+}
---
+++ supernaturalinheritancecodeacad/Storm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MagicalInheritance
+{
+    class Storm : Spell
+    {
+        public Storm(string essence, bool isStrong, string caster)
+        {
+            Caster = caster;
+            IsStrong = isStrong;
+            Essence = essence;
+        }
+
+        public override string Announce()
+        {
+            if (IsStrong == true)
+            { return $"{Caster} cast a strong {Essence} storm!"; }
+            else
+            { return $"{Caster} cast a weak {Essence} storm!"; }
+        }
+
+    }
+}
